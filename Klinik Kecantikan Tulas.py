8433904076eeee04798c26139fe4f0266b032db8
Sekar Salsabila Santosa <80144714+sekarsabil@users.noreply.github.com>
2021-06-15 10:48:34 +0700

    Update Klinik Kecantikan Tulas.py

--- Klinik Kecantikan Tulas.py
+++ Klinik Kecantikan Tulas.py
@@ -198,3 +198,203 @@ def bodytreatment():
     pilbodytreatment = input("1. Body Slimming \n2. Body Treatment \n3. Body Firming {Rp 360.000,00}")
     price_bf = 360000
     if pilbodytreatment == "1":
+        print("Silahkan pilih jenis berikut: ")
+        bodyslimming()
+    elif pilbodytreatment == "2":
+        print("Silahkan pilih jenis berikut: ")
+        treatmentbody()
+    elif pilbodytreatment == "3":
+        print("Silahkan pilih jenis berikut: ")
+        inputcustjam()
+    else:
+        print("Invalid code \nSilahkan Masukkan Sesuai Pilihan Yang Ada!")
+        bodytreatment()
+
+def bodyslimming():
+    global price_bs
+    global pilbodyslim
+    print("Berikut adalah pilihan Body Slimming \nSilahkan pilih yang anda inginkan")
+    pilbodyslim = input("1. Meso Body Perut {Rp 360.000,00}\n2. Meso Body Paha {Rp 360.000,00}\n3. Meso Body Lengan {Rp 360.000,00}\n\t:")
+    price_bs = 360000
+    if pilbodyslim == "1":
+        inputcustjam()
+    elif pilbodyslim == "2":
+        inputcustjam()
+    elif pilbodyslim == "3":
+        inputcustjam()
+    else:
+        print("Invalid code \nSilahkan Masukkan Sesuai Pilihan Yang Ada!")
+        bodyslimming()
+
+def treatmentbody():
+    global price_tb
+    print("Berikut adalah pilihan Perawatan Tubuh \nSilahkan pilih yang anda inginkan")
+    pilbodytreat = input("1. Refreshing Body Spa {Rp 330.000,00}\n2. Antioxidan Spa {Rp 330.000,00}\n3. Brightening Spa {Rp 330.000,00}\n\t:")
+    price_tb = 330000
+    if pilbodytreat == "1":
+        inputcustjam()
+    elif pilbodytreat == "2":
+        inputcustjam()
+    elif pilbodytreat == "3":
+        inputcustjam()
+    else:
+        print("Invalid code \nSilahkan Masukkan Sesuai Pilihan Yang Ada!")
+        treatmentbody()
+
+def inputcustjam():
+    global jml_custom
+    global jam_treatment
+    global sekaliguskonsul
+    jml_custom = int(input("Input Jumlah Customer \n>"))
+    print("Pilih Jam yang Anda Inginkan untuk Treatment : \n1. 08.00 \n2. 10.00 \n3. 13.00")
+    jam_treatment = input(">")
+    sekaliguskonsul = (input("Apakah Anda Ingin Sekaligus Konsultasi ? \n1. YA \n2. TIDAK \n>"))
+    print(sekaliguskonsul)
+    if sekaliguskonsul == "1" :
+        consultation(sekaliguskonsul)
+    elif sekaliguskonsul == "2" :
+        tagihan(sekaliguskonsul)
+    else:
+        print("error detected ! pilih option yang telah diberikan !")
+        inputcustjam()
+
+def consultation(tambah):
+    global harga_consul
+    global injamkonsul
+    global media
+    global indokter
+    global durasi
+    print("Berikut Daftar Jam Konsultasi :")
+    jamkonsul = "1. 09.00 \n2. 11.00 \n3. 13.00"
+    print(jamkonsul)
+    injamkonsul = (input("Pilih Jam Konsultasi Yang Anda Inginkan :"))
+    durasi = int(input("Tentukan Berapa lama konsultasi anda (dalam jam)\t:"))
+    print("Berikut Daftar Dokter :")
+    dokter = "1. Dr. Tazkiya Mutia Yogasani \n2. Dr. Sekar Salsabila Santosa \n3. Dr. Rahmat Herpradipto"
+    print(dokter)
+    indokter = (input("Pilih Dokter Yang Anda Inginkan :"))
+    print("Berikut Daftar Media Konsultasi :")
+    media = "1. Whatsapp {Rp 50.000,00} \n2. Zoom Meeting {Rp 50.000,00}\n3. Google Meeting {Rp 50.000,00}\n4. Datang Langsung {Rp 50.000,00}"
+    print(media)
+    inmedia = (input("Pilih Media Yang Anda Inginkan :"))
+    harga_consul = 50000
+    if tambah == '1':
+        tagihan('1')
+    else :
+        tagihan('3')
+
+def buymedicine():
+    global price_medicine
+    global pilmedicine
+    print("Berikut adalah beberapa medicine yang kami sediakan \nSilahkan pilih apa yang anda inginkan")
+    pilmedicine = input("1. Skincare \n2. Haircare \n\t:")
+    price_medicine = 50000
+    if pilmedicine == "1":
+        print("Silahkan pilih jenis berikut: ")
+        skincare()
+    elif pilmedicine == "2":
+        print("Silahkan pilih jenis berikut: ")
+        haircare()
+    else:
+        print("Invalid code \nSilahkan Input sesuai pilihan yang tersedia!")
+        buymedicine()
+
+def skincare():
+    global jml_skincare
+    print("Berikut adalah pilihan Skincare \nSilahkan pilih yang anda inginkan")
+    pilskincare = input("1. Serum {Rp 50.000,00}\n2. Mask {Rp 50.000,00}\n3. Toner {Rp 50.000,00}\n\t:")
+    if pilskincare == "1":
+        jml_skincare = int(input("Masukkan Jumlah produk yang anda inginkan\t:"))
+        tagihan('2')
+    elif pilskincare == "2":
+        jml_skincare = int(input("Masukkan Jumlah produk yang anda inginkan\t:"))
+        tagihan('2')
+    elif pilskincare == "3":
+        jml_skincare = int(input("Masukkan Jumlah produk yang anda inginkan\t:"))
+        tagihan('2')
+    else:
+        print("Invalid code \nSilahkan Masukkan Sesuai Pilihan Yang Ada!")
+        skincare()
+
+def haircare():
+    global jml_haircare
+    print("Berikut adalah pilihan Haircare \nSilahkan pilih yang anda inginkan")
+    pilhaircare = input("1. Shampo {Rp 50.000,00}\n2. Conditioner {Rp 50.000,00}\n\t:")
+    if pilhaircare == "1":
+        jml_haircare = int(input("Masukkan Jumlah produk yang anda inginkan\t:"))
+        tagihan()
+    elif pilhaircare == "2":
+        jml_haircare = int(input("Masukkan Jumlah produk yang anda inginkan\t:"))
+        tagihan()
+    else:
+        print("Invalid code \nSilahkan Masukkan Sesuai Pilihan Yang Ada!")
+        haircare()
+
+def cancel():
+    no_pesanan = input("Silahkan input nomor pesanan anda \t:")
+    cekpesanan(no_pesanan)
+    print("Anda akan menerima refund sebesar 70% dari yang anda bayarkan sebelumnya")
+    make_sure  = input("Apakah anda yakin ingin membatalkan pesanan? \n1. YES \n2. NO \t:")
+    if make_sure == "1":
+        print("Pesanan anda telah dibatalkan \nAnda akan menerima refund sesuai dengan media pembayaran yang anda gunakan sebelumnya paling lambat 2 x 24 jam")
+    else:
+        print("Terima Kasih Telah Memilih Produk Kami")
+    exit()
+
+
+saat_ini = dtm.now()
+sekarang = '{}'.format(dtm.strftime(saat_ini,'%d%Y%M'))
+struk = ('''
+
+=================================================
+
+                STRUK
+
+===================================================
+
+
+
+Nomor pesanan anda \t : {}'''.format(dtm.strftime(saat_ini,'%d%Y%M'))
+)
+
+penutup = ("""
+Pemesanan dilakukan pada {}""".format(dtm.strftime(saat_ini,'%d-%m-%Y %H:%M')))
+tambahan = ("""
+
+					TERIMA KASIH 
+===================================================					
+""")
+
+
+def tagihan(sekaligus):
+    print("Pesanan Anda Telah Diterima \nMohon Konfirmasi kembali pesanan dan tagihan anda")
+    def tagihantunggal():
+        if menu == "1":
+            if intreatment == "1":
+                if pilfacetreatment == "1":
+                	print("Pesanan anda berupa treatment Fruit Facial seharga Rp ",price_ff, "untuk",jml_custom,"orang \nDengan Total Tagihan = \tRp ", jml_custom * price_ff)
+                	pes = 'Pesanan anda berupa Fruit Facial '
+                	total = jml_custom * price_ff
+                elif pilfacetreatment == "2":
+                    print("Pesanan anda berupa treatment Anti Acne Facial seharga Rp ",price_af, "untuk",jml_custom,"orang \nDengan total Tagihan = \tRp ", jml_custom * price_af)
+                    pes = 'Pesanan anda berupa Anti Acne Facial '
+                    total = jml_custom * price_af
+                else:
+                    print("Pesanan anda berupa treatment Moisturizing Facial seharga Rp ", price_mf, "untuk",jml_custom,"orang \nDengan total Tagihan = \tRp ", jml_custom * price_mf)
+                    pes = 'Pesanan anda berupa Moisturizing Facial '
+                    total = jml_custom * price_mf
+            elif intreatment == "2":
+                if pilhairtreatment == "1":
+                    print("Pesanan anda berupa Medical Hair Treatment seharga Rp ", price_ht, "untuk", jml_custom, "orang \nDengan total Tagihan = \tRp ",jml_custom * price_ht)
+                    pes = 'Pesanan anda berupa Medical Hair Treatment '
+                    total = jml_custom * price_ht
+                elif  pilhairtreatment == "2":
+                    print("Pesanan anda berupa Medical Hair Mask seharga Rp ", price_ht, "untuk", jml_custom, "orang \nDengan total Tagihan = \tRp ",jml_custom * price_ht)
+                    pes = 'Pesanan anda berupa Medical Hair Mask '
+                    total = jml_custom * price_ht
+                else:
+                    print("Pesanan anda berupa Waxing seharga Rp ", price_ht, "untuk", jml_custom, "orang \nDengan total Tagihan = \tRp ",jml_custom * price_ht)
+                    pes = 'Pesanan anda berupa Waxing '
+                    total = jml_custom * price_ht
+            else:
+                if pilbodytreatment == "1":
