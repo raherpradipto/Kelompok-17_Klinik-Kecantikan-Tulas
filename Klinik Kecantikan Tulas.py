ca2c6d6aa2fb1bda6deccc6e11aa9455ca83c213
rizkyammar <80144906+rizkyammar@users.noreply.github.com>
2021-06-15 10:32:59 +0700

    Update Klinik Kecantikan Tulas.py

--- Klinik Kecantikan Tulas.py
+++ Klinik Kecantikan Tulas.py
@@ -1,3 +1,100 @@
+from datetime import datetime as dtm
+import json
+
+
+def create_json(file_name):
+    with open(file_name, 'w') as json_file:
+        json.dump([], json_file)
+
+
+def read_json(file_name):
+    with open(file_name, 'r') as json_file:
+        database = json.load(json_file)
+    return database
+
+def write_json(file_name, database, username, password, nomember):
+    account = {
+        'username': username,
+        'password': password,
+        'nomor member': nomember
+    }
+
+    database.append(account)
+
+    with open(file_name, 'w') as json_file:
+        json.dump(database, json_file)
+
+
+def awal_regis(username,password,no_member):
+    if __name__ == '__main__':
+        FILE_NAME = 'database user.json'
+
+        try:
+            database = read_json(FILE_NAME)
+        except:
+            create_json(FILE_NAME)
+            database = read_json(FILE_NAME)
+
+        USERNAME = username
+        PASSWORD = password
+        NOMEMBER = no_member
+
+        write_json(FILE_NAME, database, USERNAME, PASSWORD, NOMEMBER)
+
+
+def regispesanan(no_pesanan):
+    if __name__ == '__main__':
+        FILE_NAME = 'nomor pesanan.json'
+
+        try:
+            database = read_json(FILE_NAME)
+        except:
+            create_json(FILE_NAME)
+            database = read_json(FILE_NAME)
+
+        NO_PESANAN = no_pesanan
+
+        write_pesanan(FILE_NAME, database, NO_PESANAN)
+
+
+def write_pesanan(file_name, database, no_pesanan):
+    account = {
+        'nomor pesanan' : no_pesanan
+    }
+
+    database.append(account)
+
+    with open(file_name, 'w') as json_file:
+        json.dump(database, json_file)
+
+
+def cekpesanan(no_pesanan):
+    def checkpesanan(database, nomorpesanan):
+
+        data = {
+            'nomor pesanan' : nomorpesanan
+        }
+
+        if data in database:
+        	pass
+        else:
+            print('Nomor pesanan tidak ditemukan')
+            print('Masukkan nomor pesanan yang benar')
+            cancel()
+    if __name__ == '__main__':
+        FILE_NAME = 'nomor pesanan.json'
+
+
+        try:
+            database = read_json(FILE_NAME)
+        except:
+            create_json(FILE_NAME)
+            database = read_json(FILE_NAME)
+
+        NOMORPESANAN = no_pesanan
+
+        checkpesanan(database, NOMORPESANAN)
+
 
 def treatment():
     global intreatment
@@ -14,6 +111,7 @@ def treatment():
     else:
         print("Invaid code \nSilahkan input sesuai pilihan")
         mulai()
+
 
 def facetreatment():
     global pilfacetreatment
@@ -100,295 +198,3 @@ def bodytreatment():
     pilbodytreatment = input("1. Body Slimming \n2. Body Treatment \n3. Body Firming {Rp 360.000,00}")
     price_bf = 360000
     if pilbodytreatment == "1":
-        print("Silahkan pilih jenis berikut: ")
-        bodyslimming()
-    elif pilbodytreatment == "2":
-        print("Silahkan pilih jenis berikut: ")
-        treatmentbody()
-    elif pilbodytreatment == "3":
-        print("Silahkan pilih jenis berikut: ")
-        inputcustjam()
-    else:
-        print("Invalid code \nSilahkan Masukkan Sesuai Pilihan Yang Ada!")
-        bodytreatment()
-
-def bodyslimming():
-    global price_bs
-    global pilbodyslim
-    print("Berikut adalah pilihan Body Slimming \nSilahkan pilih yang anda inginkan")
-    pilbodyslim = input("1. Meso Body Perut {Rp 360.000,00}\n2. Meso Body Paha {Rp 360.000,00}\n3. Meso Body Lengan {Rp 360.000,00}\n\t:")
-    price_bs = 360000
-    if pilbodyslim == "1":
-        inputcustjam()
-    elif pilbodyslim == "2":
-        inputcustjam()
-    elif pilbodyslim == "3":
-        inputcustjam()
-    else:
-        print("Invalid code \nSilahkan Masukkan Sesuai Pilihan Yang Ada!")
-        bodyslimming()
-
-def treatmentbody():
-    global price_tb
-    print("Berikut adalah pilihan Perawatan Tubuh \nSilahkan pilih yang anda inginkan")
-    pilbodytreat = input("1. Refreshing Body Spa {Rp 330.000,00}\n2. Antioxidan Spa {Rp 330.000,00}\n3. Brightening Spa {Rp 330.000,00}\n\t:")
-    price_tb = 330000
-    if pilbodytreat == "1":
-        inputcustjam()
-    elif pilbodytreat == "2":
-        inputcustjam()
-    elif pilbodytreat == "3":
-        inputcustjam()
-    else:
-        print("Invalid code \nSilahkan Masukkan Sesuai Pilihan Yang Ada!")
-        treatmentbody()
-
-def consultation():
-    global harga_concul
-    global injamkonsul
-    global media
-    global indokter
-    global durasi
-    print("Berikut Daftar Jam Konsultasi :")
-    jamkonsul = "1. 09.00 \n2. 11.00 \n3. 13.00"
-    print(jamkonsul)
-    injamkonsul = (input("Pilih Jam Konsultasi Yang Anda Inginkan :"))
-    print("Berikut Daftar Dokter :")
-    dokter = "1. Dr. Tazkiya Mutia Yogasani \n2. Dr. Sekar Salsabila Santosa \n3. Dr. Rahmat Herpradipto"
-    print(dokter)
-    indokter = (input("Pilih Dokter Yang Anda Inginkan :"))
-    print("Berikut Daftar Media Konsultasi :")
-    media = "1. Whatsapp \n2. Zoom Meeting {Rp 50.000,00}\n3. Google Meeting {Rp 50.000,00}\n4. Datang Langsung {Rp 50.000,00}"
-    print(media)
-    inmedia = (input("Pilih Media Yang Anda Inginkan :"))
-    tagihan()
-
-def buymedicine():
-    global price_medicine
-    global pilmedicine
-    print("Berikut adalah beberapa medicine yang kami sediakan \nSilahkan pilih apa yang anda inginkan")
-    pilmedicine = input("1. Skincare \n2. Haircare \n\t:")
-    price_medicine = 50000
-    if pilmedicine == "1":
-        print("Silahkan pilih jenis berikut: ")
-        skincare()
-    elif pilmedicine == "2":
-        print("Silahkan pilih jenis berikut: ")
-        haircare()
-    else:
-        print("Invalid code \nSilahkan Input sesuai pilihan yang tersedia!")
-        buymedicine()
-
-def skincare():
-    global jml_skincare
-    print("Berikut adalah pilihan Skincare \nSilahkan pilih yang anda inginkan")
-    pilskincare = input("1. Serum {Rp 50.000,00}\n2. Mask {Rp 50.000,00}\n3. Toner {Rp 50.000,00}\n\t:")
-    if pilskincare == "1":
-        tagihan()
-    elif pilskincare == "2":
-        tagihan()
-    elif pilskincare == "3":
-        tagihan()
-    else:
-        print("Invalid code \nSilahkan Masukkan Sesuai Pilihan Yang Ada!")
-        skincare()
-
-def haircare():
-    global jml_haircare
-    print("Berikut adalah pilihan Haircare \nSilahkan pilih yang anda inginkan")
-    pilhaircare = input("1. Shampo {Rp 50.000,00}\n2. Conditioner {Rp 50.000,00}\n\t:")
-    if pilhaircare == "1":
-        tagihan()
-    elif pilhaircare == "2":
-        tagihan()
-    else:
-        print("Invalid code \nSilahkan Masukkan Sesuai Pilihan Yang Ada!")
-        haircare()
-
-def cancel():
-    no_pesanan = input("Silahkan input nomor pesanan anda \t:")
-    print("Anda akan menerima refund sebesar 70% dari yang anda bayarkan sebelumnya")
-    make_sure  = input("Apakah anda yakin ingin membatalkan pesanan? \n1. YES \n2. NO \t:")
-    if make_sure == "1":
-        print("Pesanan anda telah dibatalkan \nAnda akan menerima refund sesuai dengan media pembayaran yang anda gunakan sebelumnya paling lambat 2 x 24 jam")
-    else:
-        print("Terima Kasih Telah Memilih Produk Kami")
-
-def inputcustjam():
-    global jml_custom
-    global jam_treatment
-    jml_custom = int(input("Input Jumlah Customer \n>"))
-    print("Pilih Jam yang Anda Inginkan untuk Treatment : \n1. 08.00 \n2. 10.00 \n3. 13.00")
-    jam_treatment = input(">")
-    sekaliguskonsul = (input("Apakah Anda Ingin Sekaligus Konsultasi ? \n1. YA \n2. TIDAK \n>"))
-    print(sekaliguskonsul)
-    if sekaliguskonsul == "1" :
-        consultation()
-    else:
-        tagihan()
-
-def tagihan():
-    print("Pesanan Anda Telah Diterima \nMohon Konfirmasi kembali pesanan dan tagihan anda")
-    if menu == "1":
-        if intreatment == "1":
-            if pilfacetreatment == "1":
-                print("Pesanan anda berupa treatment Fruit Facial seharga Rp ",price_ff, "untuk",jml_custom,"orang \nDengan Total Tagihan = \tRp ", jml_custom * price_ff)
-            elif pilfacetreatment == "2":
-                print("Pesanan anda berupa treatment Anti Acne Facial seharga Rp ",price_af, "untuk",jml_custom,"orang \nDengan total Tagihan = \tRp ", jml_custom * price_af)
-            else:
-                print("Pesanan anda berupa treatment Moisturizing Facial seharga Rp ", price_mf, "untuk",jml_custom,"orang \nDengan total Tagihan = \tRp ", jml_custom * price_mf)
-        elif intreatment == "2":
-            if pilhairtreatment == "1":
-                print("Pesanan anda berupa Medical Hair Treatment seharga Rp ", price_ht, "untuk", jml_custom, "orang \nDengan total Tagihan = \tRp ",jml_custom * price_ht)
-            elif  pilhairtreatment == "2":
-                print("Pesanan anda berupa Medical Hair Mask seharga Rp ", price_ht, "untuk", jml_custom, "orang \nDengan total Tagihan = \tRp ",jml_custom * price_ht)
-            else:
-                print("Pesanan anda berupa Waxing seharga Rp ", price_ht, "untuk", jml_custom, "orang \nDengan total Tagihan = \tRp ",jml_custom * price_ht)
-        else:
-            if pilbodytreatment == "1":
-                print("Pesanan anda berpa Body Slimming Treatment seharga Rp ", price_bs, "untuk", jml_custom, "orang \nDengan total Tagihan = \tRp ",jml_custom * price_bs)
-            elif pilbodytreatment == "2":
-                print("Pesanan anda berpa Body Treatment seharga Rp ", price_tb, "untuk", jml_custom, "orang \nDengan total Tagihan = \tRp ",jml_custom * price_tb)
-            else:
-                print("Pesanan anda berpa Body Firming seharga Rp ", price_bf, "untuk", jml_custom, "orang \nDengan total Tagihan = \tRp ",jml_custom * price_bf)
-    elif menu == "2":
-        if injamkonsul == "1":
-            if indokter == "1":
-                if media == "1" or "2" or "3":
-                    print("Anda memesan Konsultasi secara Online bersama Dr. Tazkiya pada jam 09:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-                else:
-                    print("Anda memesan Konsultasi secara Langsung bersama Dr. Tazkiya pada jam 09:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-            elif indokter == "2":
-                if media == "1" or "2" or "3":
-                    print("Anda memesan Konsultasi secara Online bersama Dr. Sekar pada jam 09:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-                else:
-                    print("Anda memesan Konsultasi secara Langsung bersama Dr. Sekar pada jam 09:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-            else:
-                if media == "1" or "2" or "3":
-                    print("Anda memesan Konsultasi secara Online bersama Dr. Rahmat pada jam 09:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-                else:
-                    print("Anda memesan Konsultasi secara Langsung bersama Dr. Rahmat pada jam 09:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-        elif injamkonsul == "2":
-            if indokter == "1":
-                if media == "1" or "2" or "3":
-                    print("Anda memesan Konsultasi secara Online bersama Dr. Tazkiya pada jam 11:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-                else:
-                    print("Anda memesan Konsultasi secara Langsung bersama Dr. Tazkiya pada jam 11:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-            elif indokter == "2":
-                if media == "1" or "2" or "3":
-                    print("Anda memesan Konsultasi secara Online bersama Dr. Sekar pada jam 11:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-                else:
-                    print("Anda memesan Konsultasi secara Langsung bersama Dr. Sekar pada jam 11:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-            else:
-                if media == "1" or "2" or "3":
-                    print("Anda memesan Konsultasi secara Online bersama Dr. Rahmat pada jam 11:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-                else:
-                    print("Anda memesan Konsultasi secara Langsung bersama Dr. Rahmat pada jam 11:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-        else:
-            if indokter == "1":
-                if media == "1" or "2" or "3":
-                    print("Anda memesan Konsultasi secara Online bersama Dr. Tazkiya pada jam 13:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-                else:
-                    print("Anda memesan Konsultasi secara Langsung bersama Dr. Tazkiya pada jam 13:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-            elif indokter == "2":
-                if media == "1" or "2" or "3":
-                    print("Anda memesan Konsultasi secara Online bersama Dr. Sekar pada jam 13:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-                else:
-                    print("Anda memesan Konsultasi secara Langsung bersama Dr. Sekar pada jam 13:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-            else:
-               if media == "1" or "2" or "3":
-                   print("Anda memesan Konsultasi secara Online bersama Dr. Rahmat pada jam 13:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-               else:
-                   print("Anda memesan Konsultasi secara Langsung bersama Dr. Rahmat pada jam 13:00, selama ",durasi," jam dengan total tagihan \t=Rp ",durasi * harga_consul)
-    elif menu == "3":
-        if pilmedicine == "1":
-            print("Anda memesan produk Skincare sebanyak ",jml_skincare," buah, seharga Rp ",price_medicine," \nDengan total Tagihan = Rp ",jml_skincare * price_medicine)
-        else:
-            print("Anda memesan produk Haircare sebanyak ",jml_haircare," buah, seharga Rp ",price_medicine," \nDengan total Tagihan = Rp ",jml_haircare * price_medicine)
-    else:
-        pass
-    payment()
-    
-def payment():
-    print("Berikut adalah pilihan Payment \nSilahkan pilih yang anda inginkan")
-    pilpayment= input("1. Transfer Bank \n2. Ovo \n3. Dana \n\t:")
-    if pilpayment == "1":
-        transferbank()
-    elif pilpayment == "2":
-        referencenumber()
-    elif pilpayment == "3":
-        referencenumber()
-    else:
-        print("Invalid code \nSilahkan Masukkan Sesuai Pilihan Yang Ada!")
-        payment()
-        
-def transferbank():
-    print("Berikut adalah pilihan Transfer Bank \nSilahkan pilih yang anda inginkan")
-    piltfbank = input("1. BRI \n2. MANDIRI \n3. BNI \n\t:")
-    if piltfbank == "1":
-        referencenumber()
-    elif piltfbank == "2":
-        referencenumber()
-    elif piltfbank == "3":
-        referencenumber()
-    else:
-        print("Invalid code \nSilahkan Masukkan Sesuai Pilihan Yang Ada!")
-        transferbank()
-
-def referencenumber():
-    noreference = input("Silahkan masukkan nomor referensi anda:")
-    pass
-
-def mulai():
-    print("Selamat Datang di Aplikasi Klinik Tulas \nDapatkan Apa Yang Anda Inginkan",
-          "==="*12,"\nSilahkan pilih layanan yang tersedia \n1. Treatment \n2. Consultation \n3. Buy Medicine \n4. Cancel")
-    menu = input("Silahkan pilih jenis produk yang anda inginkan\t:")
-    if menu == "1":
-        treatment()
-    elif menu == "2":
-        consultation()
-    elif menu == "3":
-        buymedicine()
-    elif menu == "4":
-        cancel()
-    else:
-        print("Invalid code")
-    mulai()
-
-def SignUp():
-    username = input(str("Input Username Anda\t:"))
-    password = input(str("Input Password Anda\t:"))
-    cpassword = input(str("Masukkan ulang Password anda\t:"))
-    if password == cpassword:
-        pass
-    else:
-        print("Password anda tidak sesuai")
-    NoWhatsApp = int(input("Input Nomor WhatsApp Anda\t:"))
-    email = input(str("Input Alamat Email Anda\t:"))
-    noverif = input("Silahkan Input Nomor Verifikasi Yang Sudah Dikirimkan\t:")
-    print("Selamat Anda Telah Terdaftar Sebagai Member Klinik Tulas ")
-    member_number = print("Berikut Nomor Member Anda : ", NoWhatsApp)
-    mulai()
-
-def Login():
-    usernamelogin = input("Input Username Anda\t:")
-    passwordlogin = input("Input Password Anda\t:")
-    memberlogin = input("Input Nomor Member Anda\t:")
-    print("Login Success")
-    mulai()
-
-def start():
-    global registrasi
-    print("Selamat Datang di Program Registrasi Klinik Tulas")
-    print("===" * 12)
-    print("Silahkan Lakukan Registrasi Dibawah Ini:")
-    registrasi = input("Silahkan Pilih Langkah yang Akan Anda Lakukan \n1. Sign Up \n2. Login\t\n>")
-
-start()
-
-if registrasi == "1":
-    SignUp()
-elif registrasi == "2":
-    Login()
-else:
-    print("Invalid Kode")
